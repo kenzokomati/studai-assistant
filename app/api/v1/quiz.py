aed8f83283a3406c69d9e821eb9060b2cf8210d2
Jhonata Demuner <jhonata.demuner@gmail.com>
2025-07-06 17:16:12 -0300

    feat: create generate quiz from pdf endpoint

--- app/api/v1/quiz.py
+++ app/api/v1/quiz.py
@@ -1,7 +1,7 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, File, Form, HTTPException, UploadFile
 from app.schemas.quiz import Quiz
-from app.services.quiz_service import generate_quiz
-from app.schemas.quiz_request import QuizRequest
+from app.services.quiz_service import generate_quiz, generate_quiz_from_file
+from app.schemas.quiz_request import LanguageCode, QuizRequest, SourceType
 
 router = APIRouter()
 
@@ -9,6 +9,27 @@ router = APIRouter()
 async def generate_quiz_endpoint(request: QuizRequest):
     try:
         return generate_quiz(request)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+@router.post("/from-file", response_model=Quiz)
+async def generate_quiz_from_file_endpoint(
+    file: UploadFile = File(...),
+    sourceType: SourceType = Form(...),
+    questionsQuantity: int = Form(10),
+    languageCode: LanguageCode = Form(LanguageCode.EN),
+    startPage: int = Form(1),
+    endPage: int = Form(25)
+):
+    try:
+        return generate_quiz_from_file(
+            file=file,
+            sourceType=sourceType,
+            questionsQuantity=questionsQuantity,
+            languageCode=languageCode,
+            startPage=startPage,
+            endPage=endPage
+        )
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
  
--- app/services/quiz_service.py
+++ app/services/quiz_service.py
@@ -1,8 +1,10 @@
+from fastapi import UploadFile
 from app.schemas.quiz import Quiz
 from app.schemas.message import Message
-from app.schemas.quiz_request import QuizRequest
+from app.schemas.quiz_request import LanguageCode, QuizRequest
 from app.schemas.quiz_request import SourceType
 
+from app.services.file_service import extract_pdf_content
 from app.utils.prompts import read_prompts_file
 from app.services.transcript_service import get_transcript
 from app.clients.openai_client import requestChatCompletion
@@ -28,6 +30,34 @@ def generate_quiz(request: QuizRequest) -> Quiz:
         response_format=Quiz
     )
 
+def generate_quiz_from_file(file: UploadFile, sourceType: SourceType,
+    questionsQuantity: int, languageCode: LanguageCode,
+    startPage: int, endPage: int) -> Quiz:
+
+    parsed_request = QuizRequest(
+        sourceType=sourceType,
+        sourceContent=extract_pdf_content(file, startPage, endPage),
+        questionsQuantity=questionsQuantity,
+        languageCode=languageCode
+    )
+
+    instructions_message = Message(
+        role="system",
+        content=QUIZ_GENERATION_INSTRUCTIONS
+    )
+
+    system_message = assemble_system_message(parsed_request)
+    user_message = assemble_user_message(parsed_request)
+
+    return requestChatCompletion(
+        messages=[
+            instructions_message,
+            system_message,
+            user_message
+        ],
+        response_format=Quiz
+    )
+
 
     
 def assemble_system_message(request: QuizRequest) -> Message:
@@ -37,9 +67,12 @@ def assemble_system_message(request: QuizRequest) -> Message:
         
     elif request.sourceType == SourceType.YOUTUBE_VIDEO:
         content = f"""You are a quiz generator. Based on the following video transcript, generate a quiz with {request.questionsQuantity} questions in {request.languageCode}. The transcript may include informal speech, digressions, and filler words — ignore those and focus on extracting the key ideas."""
+
+    elif request.sourceType == SourceType.PDF_CONTENT:
+        content = f"""You are a quiz generator. Based on the following PDF content, generate a quiz with {request.questionsQuantity} questions in {request.languageCode}. The content may include informal speech, digressions, and filler words — ignore those and focus on extracting the key ideas."""
         
     else:
-        raise ValueError(f"Invalid sourceType: {request.sourceType}. Must be '{SourceType.PROMPT_BASED}' or '{SourceType.YOUTUBE_VIDEO}'.")
+        raise ValueError(f"Invalid sourceType: {request.sourceType}.")
     
     return Message(
         role="system",
@@ -58,9 +91,12 @@ def assemble_user_message (request: QuizRequest) -> Message:
             raise ValueError("Transcript not found or unavailable.")
         
         content = f'Generate a quiz with {request.questionsQuantity} questions in {request.languageCode} language based on the following transcript: "{transcript}".'
+
+    elif request.sourceType == SourceType.PDF_CONTENT:
+        content = f'Generate a quiz with {request.questionsQuantity} questions in {request.languageCode} language based on the following PDF content: "{request.sourceContent}".'
     
     else:
-        raise ValueError(f"Invalid sourceType: {request.sourceType}. Must be '{SourceType.PROMPT_BASED}' or '{SourceType.YOUTUBE_VIDEO}'.")
+        raise ValueError(f"Invalid sourceType: {request.sourceType}.")
 
     return Message(
         role="user",
